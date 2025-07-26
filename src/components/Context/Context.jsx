b47e4136093375fc6797ac6ef1e33877c0a87356
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-07-26 17:34:41 +0200

    // Function to fetch all products from Fake Store API

--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -7,6 +7,23 @@ export default function StoreContextProvider({ children }) {
     
     // State to store the error message (if any)
     const [errorMessage, setErrorMessage] = useState("");
+
+    const [products, setProducts] = useState([]);
+
+    // Function to fetch all products from Fake Store API
+    async function getAllProducts() {
+        setLoading(true);
+        setErrorMessage("");
+        try {
+            const response = await axios.get("https://fakestoreapi.com/products");
+            setProducts(response.data);
+        } catch (error) {
+            setErrorMessage("error");
+        } finally {
+            setLoading(false);
+        }
+    };
+    
     return (
         <dataContext.Provider>
             {children}
