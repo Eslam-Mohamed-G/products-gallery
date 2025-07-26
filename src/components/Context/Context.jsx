b62f4fa8c03ed084ca17f9605edc9b912bfe6cdd
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-07-26 16:19:35 +0200

    Use the Context API to provide a way to share data (state) between multiple components without having to pass props repeatedly (prop drilling).

--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -2,10 +2,11 @@ import React, { createContext } from 'react'
 
 export const dataContext = createContext();
 
-export default function StoreContextProvider() {
-  return (
-    <dataContext.Provider>
-        {children}
-    </dataContext.Provider>
-  )
+export default function StoreContextProvider({ children }) {
+    
+    return (
+        <dataContext.Provider>
+            {children}
+        </dataContext.Provider>
+    )
 }
