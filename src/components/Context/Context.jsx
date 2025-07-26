f814b9a1288587a97f882e2b8bd25b3ecfd2ed19
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-07-26 17:30:35 +0200

    State to store the error message (if any)

--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -4,6 +4,8 @@ export const dataContext = createContext();
 
 export default function StoreContextProvider({ children }) {
     const [loading, setLoading] = useState(false);
+    
+    // State to store the error message (if any)
     const [errorMessage, seterrorMessage] = useState("");
     return (
         <dataContext.Provider>
