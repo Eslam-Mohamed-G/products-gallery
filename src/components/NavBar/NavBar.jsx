6376e11c34239ee6280a88ce4265fc6c72d806a4
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-07-27 19:12:10 +0200

    handle navbar content to be responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react'
 import { useEffect, useState } from 'react'
 import { dataContext } from '../Context/Context';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function NavBar() {
+    const location = useLocation();
     const { getAllProducts, products } = useContext(dataContext);
     const [mode, setMode] = useState(() => {
         return localStorage.getItem("theme") === "dark";
@@ -30,7 +31,8 @@ export default function NavBar() {
                 </Link>
 
                 <div className="flex items-center content-center">
-                    <div className="relative md:w-80">
+                    {/* search bar */}
+                    <div className={`relative md:w-80 ${location.pathname === '/' ? 'opacity-100' : 'opacity-0'}`}>
                         <div className="absolute inset-y-0 end-0 flex items-center pr-3 pointer-events-none">
                             <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
