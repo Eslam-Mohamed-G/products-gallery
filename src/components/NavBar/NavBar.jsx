8da62717a716e6c7cc4430b24d139ae838c179e6
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-07-27 20:50:29 +0200

    Search when clicking on the search icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -26,6 +26,12 @@ export default function NavBar() {
             handleSearch(term);
         }
     };
+    
+    // Search when clicking on the search icon
+    const handleSearchClick = () => {
+        const term = searchInputRef.current.value.trim();
+        handleSearch(term);
+    };
 
     useEffect(() => {
         document.querySelector("html").classList.toggle("dark", mode);
@@ -41,11 +47,11 @@ export default function NavBar() {
                 <div className="flex items-center content-center">
                     {/* search bar */}
                     <div className={`relative md:w-80 ${location.pathname === '/' ? 'opacity-100' : 'opacity-0'}`}>
-                        <div className="absolute inset-y-0 end-0 flex items-center pr-3 pointer-events-none">
+                        <button onClick={handleSearchClick} className="absolute inset-y-0 end-0 flex items-center mx-1 my-1 px-2 cursor-pointer hover:bg-gray-300 transition-all duration-300 rounded-lg">
                             <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
-                        </div>
+                        </button>
                         <input
                             type="text"
                             id="search-navbar"
