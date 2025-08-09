40ffcaeec028411ac8908cfbc270548502069703
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-08-09 13:41:30 +0200

    add CarouselData for carousel content

---
+++ src/components/Carousel/CarouselData.js
@@ -0,0 +1,50 @@
+export const carouselContent = [
+    {
+        id: 3,
+        title: "Mens Cotton Jacket",
+        price: 55.99,
+        description: "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+        category: "men's clothing",
+        image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png",
+        rating: {
+            rate: 4.7,
+            count: 500
+        }
+    },
+    {
+        id: 6,
+        title: "White Gold Plated Princess",
+        price: 9.99,
+        description: "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
+        category: "jewelery",
+        image: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_t.png",
+        rating: {
+            rate: 3,
+            count: 400
+        }
+    },
+    {
+        id: 14,
+        title: "Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultrawide Screen QLED ",
+        price: 999.99,
+        description: "49 INCH SUPER ULTRAWIDE 32:9 CURVED GAMING MONITOR with dual 27 inch screen side by side QUANTUM DOT (QLED) TECHNOLOGY, HDR support and factory calibration provides stunningly realistic and accurate color and contrast 144HZ HIGH REFRESH RATE and 1ms ultra fast response time work to eliminate motion blur, ghosting, and reduce input lag",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_t.png",
+        rating: {
+            rate: 2.2,
+            count: 140
+        }
+    },
+    {
+        id: 20,
+        title: "DANVOUY Womens T Shirt Casual Cotton Short",
+        price: 12.99,
+        description: "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
+        category: "women's clothing",
+        image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_t.png",
+        rating: {
+            rate: 3.6,
+            count: 145
+        }
+    }
+]
