2c36d827d5e967579c5e426ed13b6a6f56022552
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-08-15 18:49:05 +0200

    add category page content

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -53,16 +53,17 @@ const cardData = [
 
 export default function Category() {
     return (
-        <div className='flex'>
+        <div className='flex flex-row flex-wrap justify-center px-4 pt-4'>
             {cardData.map((item) => (
-                <div key={item.id} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img className="p-8 rounded-t-lg" src={item.image} alt={item.title} />
-                    </a>
-                    <div className="px-5 pb-5 flex justify-between items-center">
-                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{item.category}</h5>
-                        <div className="flex items-center justify-between">
-                            <button className="flex items-center justify-center capitalize text-white bg-gray-400 rounded-md px-4 py-2">
+                <div className="w-1/2 lg:w-1/4 p-2 md:p-4" key={item.id}>
+                    <div className="flex flex-col items-center justify-center h-64 md:h-96 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 cursor-pointer transition-colors ease-in-out duration-700">
+                        <div className='w-full h-1/2 sm:h-2/3 m-auto bg-transparent px-4'>
+                            <img className="object-contain w-full h-full" src={item.image} alt={item.category} />
+                        </div>
+
+                        <div className="flex flex-wrap flex-row items-center justify-between gap-4 px-2 md:px-5 pb-5 w-full">
+                            <p className="text-lg md:text-xl font-semibold tracking-tight text-gray-900 dark:text-white m-auto transition-colors ease-in-out duration-700">{item.category}</p>
+                            <button className="flex items-center justify-center capitalize text-white bg-gray-400 rounded-md px-4 py-2 m-auto transition-colors ease-in-out duration-700">
                                 more
                                 <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 5h12m0 0L9 1m4 4L9 9" />
