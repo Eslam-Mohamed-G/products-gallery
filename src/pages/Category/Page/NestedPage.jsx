f1fa99034724813d26cf78b5cfe813ab3a5c51a0
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-08-16 23:12:44 +0200

    Whenever the URL path changes, extract the category name and update the context state with it

--- src/pages/Category/Page/NestedPage.jsx
+++ src/pages/Category/Page/NestedPage.jsx
@@ -1,9 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { dataContext } from '../../../Context/Context';
 import MapContent from '../../../components/MapContent/MapContent';
+import { useLocation } from 'react-router-dom';
 
 export default function NestedPage() {
-  const { categoryFilteredProducts } = useContext(dataContext);
+  const { setCategoryName, categoryFilteredProducts } = useContext(dataContext);
+  const location = useLocation();
+
+  useEffect(() => {
+    const categoryFromPath = decodeURIComponent(location.pathname.split("/")[2]); 
+    setCategoryName(categoryFromPath);
+  }, [location.pathname]);
   return (
     <div className='flex flex-row flex-wrap justify-center'>
       {categoryFilteredProducts.map(product => (
