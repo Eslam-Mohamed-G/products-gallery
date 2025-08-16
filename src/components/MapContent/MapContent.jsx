bf7ebfe7d435d5d0794e635712e74099654e7e59
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-08-16 22:28:12 +0200

    add MapContent component

---
+++ src/components/MapContent/MapContent.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function MapContent({ id, title, image, rate, price}) {
+    return (
+        <div className="w-1/2 md:w-1/3 lg:w-1/4 p-2 md:p-4">
+            <div onClick={() => { navigate(`/${title.split(' ').slice(0, 3).join(" ")}/${id}`) }} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 h-80 md:h-96 pt-4 cursor-pointer">
+                <div className='w-2/3 h-1/2 sm:h-2/3 m-auto bg-transparent'>
+                    <img className="object-contain w-full h-full" src={image} alt="product image" />
+                </div>
+                <div className="px-5 pb-5">
+                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{title.split(' ').slice(0, 3).join(" ")}</h5>
+                    <div className="flex flex-wrap items-center justify-between mt-2.5 mb-5">
+                        <div className='flex items-center justify-center'>
+                            <span
+                                className="star-rating"
+                                style={{ '--rating-percent': `${(rate / 5) * 100}%` }}
+                            >
+                                ★★★★★
+                            </span>
+                            <span className="bg-blue-100 text-blue-800 text-[10px] font-semibold px-2 py-0.5 rounded-sm dark:bg-blue-200 dark:text-blue-800 ms-1">{rate}</span>
+                        </div>
+                        <span className="text-lg md:text-2xl lg:text-3xl font-bold text-gray-900 dark:text-white">${price}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import ErrorPage from '../ErrorPage/ErrorPage';
 import Loading from '../../components/Loading/Loading';
 import SearchInput from '../../components/SearchInput/SearchInput';
+import MapContent from '../../components/MapContent/MapContent';
 
 
 export default function Products() {
@@ -53,28 +54,7 @@ export default function Products() {
                     <div className="flex flex-row flex-wrap justify-center">
                         {filteredProducts.length > 0 ?
                             sortedProducts.map((product) => (
-                                <div className="w-1/2 md:w-1/3 lg:w-1/4 p-2 md:p-4" key={product.id}>
-                                    <div onClick={() => { navigate(`/${product.title.split(' ').slice(0, 3).join(" ")}/${product.id}`) }} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 h-80 md:h-96 pt-4 cursor-pointer">
-                                        <div className='w-2/3 h-1/2 sm:h-2/3 m-auto bg-transparent'>
-                                            <img className="object-contain w-full h-full" src={product.image} alt="product image" />
-                                        </div>
-                                        <div className="px-5 pb-5">
-                                            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{product.title.split(' ').slice(0, 3).join(" ")}</h5>
-                                            <div className="flex flex-wrap items-center justify-between mt-2.5 mb-5">
-                                                <div className='flex items-center justify-center'>
-                                                    <span
-                                                        className="star-rating"
-                                                        style={{ '--rating-percent': `${(product.rating.rate / 5) * 100}%` }}
-                                                    >
-                                                        ★★★★★
-                                                    </span>
-                                                    <span className="bg-blue-100 text-blue-800 text-[10px] font-semibold px-2 py-0.5 rounded-sm dark:bg-blue-200 dark:text-blue-800 ms-1">{product.rating.rate}</span>
-                                                </div>
-                                                <span className="text-lg md:text-2xl lg:text-3xl font-bold text-gray-900 dark:text-white">${product.price}</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <MapContent key={product.id} id={product.id} title={product.title} image={product.image} rate={product.rating.rate} price={product.price}/>
                             ))
                             :
                             <p>no products</p>
