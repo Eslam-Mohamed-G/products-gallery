9f2da1d793849387d86c10dd659d07a0674260db
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-08-16 22:37:45 +0200

    add NestedPage content

--- src/pages/Category/Page/NestedPage.jsx
+++ src/pages/Category/Page/NestedPage.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { dataContext } from '../../../Context/Context';
+import MapContent from '../../../components/MapContent/MapContent';
 
 export default function NestedPage() {
+  const { categoryFilteredProducts } = useContext(dataContext);
   return (
-    <div>oneCategory</div>
+    <div>
+      {categoryFilteredProducts.map(product => (
+        <MapContent key={product.id} id={product.id} title={product.title} image={product.image} rate={product.rating.rate} price={product.price} />
+      ))}
+    </div>
   )
 }
